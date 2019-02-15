7c764cbd80b11de5a94a3750b67c51482fdf60ff
Ben Yu <intenex@gmail.com>
2019-02-15 18:58:15 +0900

    refactored some methods to make more shorter and more elegant

--- W1D4/minesweeper/board.rb
+++ W1D4/minesweeper/board.rb
@@ -5,27 +5,22 @@ class Board
 
     attr_reader :grid # so that the tile class can access this - internal instance methods can access attributes of that class natively, but other class instance methods can't, right, of course that's how it works remember that
 
-    def initialize
-        @grid = populate_grid
-        set_tile_values # things to run as soon as the board is set up shouldn't have to automatically do it right love it know where to put everything awesome
+    def initialize # damn learning that you can chain multi lines of code onto one line with semicolons is so lit lol
+        @grid = populate_grid; set_tile_values # things to run as soon as the board is set up shouldn't have to automatically do it right love it know where to put everything awesome
     end
 
     def populate_grid
         grid_arr = Array.new(9) { Array.new(9) }
-        grid_arr.each_with_index do |row, row_i|
-            row.each_with_index do |col, col_i| # could possibly just set col = the Tile but check that later
-                grid_arr[row_i][col_i] = Tile.new(self, [row_i, col_i], (rand < 0.1)) # rand with no argument returns a random crazy floating point between 0 and 1 so under .1 is 10% of hte time, awesome. Random number generation is such an incredible thing to be able to create pseudorandomness || rand(10) % 10 should give a 10% chance of equaling 0 and the == 0 returns a boolean, so good shorthand for random probabilities, that's nice, so you get 10%. Okay nevermind just read the better way to do this lol so your original code (rand(10)%10 == 0), while totally functional, is now deprecated for being slightly less efficient and elegant.
-            end
-        end
+        grid_arr.each_with_index { |row, row_i| row.each_index { |col_i| grid_arr[row_i][col_i] = Tile.new(self, [row_i, col_i], (rand < 0.1)) } } # could possibly just set col = the Tile but check that later. Nope, confirmed this doesn't work, awesome, your intuitions are right, don't know what Theo was testing | # rand with no argument returns a random crazy floating point between 0 and 1 so under .1 is 10% of hte time, awesome. Random number generation is such an incredible thing to be able to create pseudorandomness || rand(10) % 10 should give a 10% chance of equaling 0 and the == 0 returns a boolean, so good shorthand for random probabilities, that's nice, so you get 10%. Okay nevermind just read the better way to do this lol so your original code (rand(10)%10 == 0), while totally functional, is now deprecated for being slightly less efficient and elegant.
         grid_arr
     end
 
     def set_tile_values
-        @grid.each { |subgrid| subgrid.each { |tile| tile.value = tile.neighbor_bomb_count(tile.pos) } } # set the values of each tile to how many neighbors it has now that the whole grid has been correctly created
+        @grid.each { |row| row.each { |tile| tile.value = tile.neighbor_bomb_count(tile.pos) } } # set the values of each tile to how many neighbors it has now that the whole grid has been correctly created
     end
 
     def render
-        @grid.each { |subgrid| subgrid.each { |tile| print "#{tile.to_s} " }; puts } # puts at the end to create a new line. Interesitng that the semi colon does work in ruby to put multiple lines of code on one line, interesting, just guessed it as the logical thing and it does indeed work https://stackoverflow.com/a/3953856/674794
+        @grid.each { |row| row.each { |tile| print "#{tile.to_s} " }; puts } # puts at the end to create a new line. Interesitng that the semi colon does work in ruby to put multiple lines of code on one line, interesting, just guessed it as the logical thing and it does indeed work https://stackoverflow.com/a/3953856/674794
     end
 
 end
--- W1D4/minesweeper/tile.rb
+++ W1D4/minesweeper/tile.rb
@@ -4,12 +4,7 @@ class Tile
     attr_accessor :revealed, :flagged, :value
 
     def initialize(board, pos, bomb) # true or false
-        @board = board # right need to call the getter method of board's grid later, board being the object that was just passed in...but also can't do this at exactly the same time. Have to define it later after the board is fully populated, right.
-        @bomb = bomb
-        @revealed = false
-        @flagged = false
-        @pos = pos
-        @value = nil # later call neighbor_bomb_count to change this value for everything
+        @board = board; @bomb = bomb; @revealed = false; @flagged = false; @pos = pos; @value = nil  # right need to call the getter method of board's grid later, board being the object that was just passed in...but also can't do this at exactly the same time. Have to define it later after the board is fully populated, right. || # start by setting @value to nil then later call neighbor_bomb_count to change this value for everything
     end
 
     # def inspect learn how to make this work correctly lol
@@ -21,12 +16,8 @@ class Tile
     end
 
     def neighbor_bomb_count(pos) # just to make it explicit and not rely on hard coded values again
-        neighbor_tile_locations = neighbors(pos)
-        count = 0
-        neighbor_tile_locations.each do |loc|
-            row, col = loc
-            count += 1 if (@board.grid[row][col]).bomb # if this value is true --> note that it's actually the index of the @board.grid attribute that you're calling with @grid's getter method, since @board is actual the board object you passed in, since you couldn't pass in the grid while it was being formed, you needed to wait until every Tile was initialized first and then populate these values, it has to be a two step process. So you pass in the Board object first, wait for the grid to be instantiated with tiles completely first, then call the grid from within that object passed in (a pointer to it, at least) to see how many other tiles are around it and what values they have, awesome, love it.
-        end
+        neighbor_tile_locations = neighbors(pos); count = 0
+        neighbor_tile_locations.each { |loc| row, col = loc; count += 1 if (@board.grid[row][col]).bomb }# if this value is true --> note that it's actually the index of the @board.grid attribute that you're calling with @grid's getter method, since @board is actual the board object you passed in, since you couldn't pass in the grid while it was being formed, you needed to wait until every Tile was initialized first and then populate these values, it has to be a two step process. So you pass in the Board object first, wait for the grid to be instantiated with tiles completely first, then call the grid from within that object passed in (a pointer to it, at least) to see how many other tiles are around it and what values they have, awesome, love it.
         count
     end
 
@@ -41,21 +32,16 @@ class Tile
     # [2,0], [2,1], [2,2]
 
     def neighbors(pos) # ah this is the helper method to get the neighbors right love it
-        row, col = pos
-        neighbor_tiles = Array.new # find all 8 neighbors by doing each row sequentially and getting those positions. Do three enumerators to get the top row, the middle row, and the bottom row respectively
+        neighbor_tiles = Array.new; row, col = pos # find all 8 neighbors by doing each row sequentially and getting those positions. Do three enumerators to get the top row, the middle row, and the bottom row respectively
         (-1..1).each { |offset| neighbor_tiles << [(row-1), (col+offset)] if (row-1) >= 0 && (col+offset) >= 0 && (row-1) < 9 && (col+offset) < 9 } # make sure both are within bounds and not at an edge of the board, if it's within bounds, add that position to the neighbor_tiles array to return to be checked
-        [-1,1].each { |offset| neighbor_tiles << [row, (col+offset)] if (col+offset) >= 0 && (col+offset) < 9 } # don't need a 0 here because 0 is just the current tile itself. For the enumerator's conditional check, don't just blindly copy and paste code, obviously row is fine because the current tile is in row, just check what isn't possibly fine
+        [-1, 1].each { |offset| neighbor_tiles << [row, (col+offset)] if (col+offset) >= 0 && (col+offset) < 9 } # don't need a 0 here because 0 is just the current tile itself. For the enumerator's conditional check, don't just blindly copy and paste code, obviously row is fine because the current tile is in row, just check what isn't possibly fine
         (-1..1).each { |offset| neighbor_tiles << [(row+1), (col+offset)] if (row+1) >= 0 && (col+offset) >= 0 && (row+1) < 9 && (col+offset) < 9 }
         neighbor_tiles
     end
 
     def to_s
-        if @revealed
-            if @value != 0
-                @value.to_s # send the string value of the count of number of bombs nearby
-            else # if there are no bombs nearby then it's an interior square so print "_"
-                "_"
-            end
+        if @revealed # not checked since nothing revealed yet, see if this works once revealed
+            @value != 0 ? @value.to_s : "_" # this is how to do a one line if statement love it https://stackoverflow.com/a/3827879/674794 if @value is not equal to 0 is true, then @value.to_s, otherwise if it is false and @value is 0, then print "_" to symbolize it's an interior piece surrounded by no bombs # send the string value of the count of number of bombs nearby || # if there are no bombs nearby then it's an interior square so print "_"
         elsif @flagged # if the square is revealed, that overwrites a flag and comes first, otherwise if flagged, display it as F
             "F"
         else # if not revealed or flagged, then "*" stands for just unexplored
