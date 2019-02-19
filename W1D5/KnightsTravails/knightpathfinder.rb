f0b8e3cf1a4cf2102ff8eef61085da7960c2439e
Ben Yu <intenex@gmail.com>
2019-02-18 18:22:15 -0800

    refactored correct valid_moves(pos)

--- W1D5/KnightsTravails/knightpathfinder.rb
+++ W1D5/KnightsTravails/knightpathfinder.rb
@@ -6,13 +6,40 @@ require_relative 'polytreenode'
 class KnightPathFinder # oh fantastic this is from chess love it
     def initialize(pos)
         @pos = pos
-        @considered_positions = Array.new
-        @root_node = PolyTreeNode
+        @considered_positions = [pos]
+        @root_node = PolyTreeNode.new(pos)
+        @move_tree = build_move_tree
     end
 
+    # all valid moves for a knight are 1 displacement vertically or horizontally and 2 in the other direction, 8 total possible moves
     def self.valid_moves(pos)
+        row, col = pos
+        possible_moves = Array.new
+        pos_displacements = [[-1, -2], [-1, 2], [1, -2], [1, 2], [-2, -1], [2, -1], [-2, 1], [2, 1]]
+        pos_displacements.each do |displacement|
+            x, y = displacement
+            if row+x >= 0 && row+x < 8 && col+y >= 0 && col+y <8
+                possible_moves << [row+x, col+y]
+            end
+        end
+        possible_moves
     end
+
+    # super smart on the array to iterate over with the displacements damn Alan is good
+    # possible_moves << [row-1, col-2] if row-1 >= 0 && col-2 >= 0
+    # possible_moves << [row-1, col+2] if row-1 >= 0 && col+2 < 8
+    # possible_moves << [row+1, col-2] if row+1 < 8 && col-2 >= 0
+    # possible_moves << [row+1, col+2] if row+1 < 8 && col+2 < 8
+    # possible_moves << [row-2, col-1] if row-2 >= 0 && col-1 >= 0
+    # possible_moves << [row+2, col-1] if row+2 < 8 && col-1 >= 0
+    # possible_moves << [row-2, col+1] if row-2 >= 0 && col+1 < 8
+    # possible_moves << [row+2, col+1] if row+2 < 8 && col+1 >= 8
 
     def new_move_positions(pos)
+
     end
+
+    def build_move_tree
+    end
+
 end
