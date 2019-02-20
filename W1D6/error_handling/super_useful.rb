b34f0660ac672bcf74d3dac1ca6cd196e0c61cf7
Ben Yu <intenex@gmail.com>
2019-02-19 18:26:11 -0800

    finished feed_me_a_fruit

--- W1D6/error_handling/super_useful.rb
+++ W1D6/error_handling/super_useful.rb
@@ -5,15 +5,20 @@ rescue ArgumentError # should be a type error...nope nevermind it's an ArgumentE
   nil
 end
 
+# wow if you catch Exception it'll ignore even system errors? amazing might be able to do that for stack overflows look into it more hmm
+# 
+
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+  elsif maybe_fruit.downcase == "coffee"
+    raise TypeError
+  else
+    raise StandardError
+  end
 end
 
 def feed_me_a_fruit
@@ -22,6 +27,12 @@ def feed_me_a_fruit
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
   reaction(maybe_fruit) 
+  # remember that you can just put rescues here in the code amazing
+rescue TypeError # amazing the error bubbles up from the method being called below into here and you can just rescue here and retry here --> if you rescued and retried in the submethod #reaction it would have caused an infinite loop as you found out since you never altered the fruit argument being passed in (and it was indeed just passed in again as you found out too since the method just reruns from the beginning of the block I suppose which makes sense so amazing)
+  puts "Sorry, that was coffee, not a fruit. Please try again."
+  retry
+rescue StandardError
+  puts "That wasn't a fruit :(.\nPeople like you are why monsters aren't friendly :(."
 end  
 
 # PHASE 4
