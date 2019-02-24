76626d5d513d1067d56e41ea52c0c7d623d44c4c
Ben Yu <intenex@gmail.com>
2019-02-23 20:14:01 -0800

    Added most of the logic, just need to process piece selection currently with Cursor#get_input and correctly move the right piece and ensure you can only move pieces of the same color

--- W2D1/chess3/humanplayer.rb
+++ W2D1/chess3/humanplayer.rb
@@ -1,3 +1,7 @@
+# the literal last step is processing cursor input
+# make sure that the cursor can appropriately select pieces and move them
+# make sure that you can only move pieces of your own color, and that's literally the game lol insane so easy when everything is in the right place so lucky to be done with this and to be so happy with it and for it to look so good thanks to victoria
+
 class HumanPlayer
     def initialize(color, display)
         @color = color
@@ -5,5 +9,11 @@ class HumanPlayer
     end
 
     def make_move(board)
+        system('clear')
+        self.render
+        @cursor.get_input # when you process this input make sure that the player can only select and move a piece of their color
+    rescue => e # if any of the wrong move exceptions come up handle it and retry love it
+        puts e
+        retry
     end
 end
