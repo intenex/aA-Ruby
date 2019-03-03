ae08be92b3e1e05c8ab55a0a7553d539b5c3a125
Ben Yu <intenex@gmail.com>
2019-03-03 15:15:59 -0800

    added the #game_over? checker to game

--- W2D2/Poker/lib/game.rb
+++ W2D2/Poker/lib/game.rb
@@ -8,5 +8,16 @@ class Game
         @players = players.map { |player_name| Player.new(player_name, @deck, 1000) } # starting chips of 1000 per player
         @current_bet = 0
         @pot = 0
+        @dealer = player[0]
+        @current_player = player[1] # starts as the player next to the dealer then increments and then both dealer and player shift forward one at the reset of each turn
+    end
+
+    def play_game
+        until game_over?
+        end
+    end
+
+    def game_over?
+        @players.one? { |player| player.chips != 0 } # when there is only one player left with any chips the game is over otherwise it continues love it wow literally just so much programming work to finish anything actually hilarious that they expect us to be able to finish all of these in a single day lmao
     end
 end
