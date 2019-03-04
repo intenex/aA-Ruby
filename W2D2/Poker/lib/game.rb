58480cfc00be4b9e5598d2d86137a92f9e3aad20
Ben Yu <intenex@gmail.com>
2019-03-03 23:44:45 -0800

    refactored out the reset_round method

--- W2D2/Poker/lib/game.rb
+++ W2D2/Poker/lib/game.rb
@@ -1,4 +1,6 @@
 # don't forget to account for the ante at one point too
+# your game is way over-engineered for edge cases that can never realistically happen because there are only 2 turns because you didn't realize that about five card draw
+# it is indeed way easier to just play a game you actually know lol
 
 class Game
     attr_accessor :current_bet, :pot
@@ -26,7 +28,13 @@ class Game
     def play_round
         active_players = @players.select { |player| player.chips != 0 }.compact # instead of removing the players when they run out of cards you do this maybe it's better to just remove the players lol oh well
         active_players.each { |player| player.hand.cards << @deck.get_card } until active_players.all? { |player| player.hand.cards == 5 } # until all players have 5 cards --> this simulates the correct dealing of cards of one at a time in order to all the cards
+        original_bet = @current_bet
         remaining_players = play_turn(active_players)
+        if @current_bet == original_bet # aka if everyone checked and no one bet, then restart the game, throw in all the cards, shuffle the deck, change the dealers, etc
+            players.each { |player| player.reset_hand }
+            self.reset_round
+            play_round # restart hell yeah
+        end
         discard_cards(remaining_players)
         final_players = play_turn(remaining_players)
         if final_players.length > 1 # if there's more than one player then show cards
@@ -38,6 +46,10 @@ class Game
             sleep(1)
         end
         active_players.each { |player| player.reset_hand } # this is the only time all players have their hands reset maybe you should do it when they fold and put their cards back in the discard pile then but this should work fine
+        self.reset_round
+    end
+
+    def reset_round
         @deck.reset_deck
         new_remaining_players = @players.select { |player| player.chips != 0 }.compact # okay you're doing this enough that you really should just fucking move everyone into a new array when they leave the game let's do that later sigh
         @dealer = (@dealer + 1) % new_remaining_players.length # this makes sure the dealer wraps around to 0 when there are 5 players on 5 it'll modulo to 0 love it so it'll do 0 1 2 3 4 perfect
