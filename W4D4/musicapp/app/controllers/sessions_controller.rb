3175e46ce8d5b234adc0a8fb89d814d5807c5349
Ben Yu <intenex@gmail.com>
2019-04-04 12:37:23 -0700

    Add more view stuff

--- W4D4/musicapp/app/controllers/sessions_controller.rb
+++ W4D4/musicapp/app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
     before_action :only_if_logged_in, only: [:destroy]
 
     def new
+        @user = User.new # a dummy user so you can reuse the users form for the login page lol
         render :new
     end
 
