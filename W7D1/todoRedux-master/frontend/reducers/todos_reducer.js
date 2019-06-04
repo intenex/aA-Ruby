ab6928f58fcc0e36bc73e76239b8fcf0498c3366
Ben Yu <intenex@gmail.com>
2019-06-03 22:08:43 -0700

    Yeah really don't think you need that in there removed the newTodo in the Object.assign thing

--- W7D1/todoRedux-master/frontend/reducers/todos_reducer.js
+++ W7D1/todoRedux-master/frontend/reducers/todos_reducer.js
@@ -15,16 +15,17 @@ const initialState = {
   }
 };
 
+// remember the state returned here is only actually for a slice of state not the entire state 
 export default function todosReducer(state = initialState, action) { // if there is no state passed in set it to the default state this is true the first time this thing is called
   switch (action.type) {
     case RECEIVE_TODO: // if receiving a single todo
       const newTodo = action.todo; // the todo will be the object namespaced on the action object as .todo
-      const newState = Object.assign({}, state, newTodo); // Object.assign will assign all the values of state to the {} empty POJO and then for some reason pass the newTodo values over too to the {} not sure why or if that's correct hmm
+      const newState = Object.assign({}, state); // Object.assign will assign all the values of state to the {} empty POJO and then for some reason pass the newTodo values over too to the {} not sure why or if that's correct hmm
       newState[newTodo.id] = newTodo; // this is correct --> this sets the correct attribute/key on newState to the value of the newTodo object
       return newState;
     case RECEIVE_TODOS:
-      const newTodos = {};
-      action.todos.forEach(el => {
+      const newTodos = {}; // this is the entirety of the new slice of todos state // look more into hoisting of variables and all that for sure
+      action.todos.forEach(el => { // yep for each todo set the new state's 
         newTodos[el.id] = el;
       });
       return newTodos;
