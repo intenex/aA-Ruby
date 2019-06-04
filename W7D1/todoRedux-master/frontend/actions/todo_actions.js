04560bc00a1f4195a30d6ce8b5ac5fc93d61e394
Ben Yu <intenex@gmail.com>
2019-06-03 21:29:25 -0700

    Testing

--- W7D1/todoRedux-master/frontend/actions/todo_actions.js
+++ W7D1/todoRedux-master/frontend/actions/todo_actions.js
@@ -1,10 +1,12 @@
 export const RECEIVE_TODOS = "RECEIVE_TODOS";
 export const RECEIVE_TODO = "RECEIVE_TODO";
 
+// to receive just one todo
 export const receiveTodo = todo => {
   return { type: RECEIVE_TODO, todo };
 };
 
+// to receive multiple todos at once yep
 export const receiveTodos = todos => {
   return { type: RECEIVE_TODOS, todos };
 };
