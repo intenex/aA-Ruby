206c74eeb698e61c1294b42836778d6c657e60e8
Ben Yu <intenex@gmail.com>
2019-06-07 21:28:08 -0700

    Fully fixed lol

--- W7D1/todoRedux-master/frontend/components/todo_list/todo_form.jsx
+++ W7D1/todoRedux-master/frontend/components/todo_list/todo_form.jsx
@@ -4,7 +4,7 @@ class TodoForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      todo: ''
     };
     this.handleChange = this.handleChange.bind(this); // right have to bind this because it's going to be called function style by the component which is passed this as a callback uninvoked yep love it
     this.addTodo = this.addTodo.bind(this);
@@ -13,13 +13,17 @@ class TodoForm extends React.Component {
   // basically whenever the text changes the state will change
   // and reflect the current todo value then you can use that
   // in the submit trigger
+  // right it rerenders every time so you have to save it
+  // as the new value love it heh
+  // nope lol you fixed it now
   handleChange(event) {
+    console.log(event.target.value);
     this.setState({todo: event.target.value});
   }
 
   addTodo(event) {
     event.preventDefault(); // stop it from actually reloading the page
-    console.log(this.state.value);
+    console.log(this.state.todo);
   }
 
   render() { // oh fuck yeah this is exactly as far as you got with your code let's make new changes now :)
@@ -27,7 +31,7 @@ class TodoForm extends React.Component {
       <form onSubmit={this.addTodo}>
         <label>Todo:
           <input type="text"
-            value={this.state.value}
+            value={this.state.todo}
             onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
