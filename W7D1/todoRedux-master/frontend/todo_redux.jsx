2572cf0598c3b361b370ac5f6da8d4648915150d
Ben Yu <intenex@gmail.com>
2019-06-08 15:48:30 -0700

    Remove all the window adding that was unnecessary

--- W7D1/todoRedux-master/frontend/todo_redux.jsx
+++ W7D1/todoRedux-master/frontend/todo_redux.jsx
@@ -6,9 +6,6 @@ import { allTodos } from './reducers/selectors';
 
 // yep this is the entry file now you actually know all your code fucking dope
 const store = configureStore();
-window.store = store;
-window.allTodos = allTodos;
-window.React = React; // shouldn't need this line but something breaks in todo_list_container and todo_list without it look into it more hmm
 
 document.addEventListener("DOMContentLoaded", (event) => {
     const rootElement = document.getElementById('root');
