c6b21f0854ad51925d474b7b2baaaa15336d33cb
Ben Yu <intenex@gmail.com>
2019-06-08 15:45:00 -0700

    Solved all your issues lmao it WAS working bc this shit was put on the window so nuts

--- W7D1/todoRedux-master/frontend/todo_redux.jsx
+++ W7D1/todoRedux-master/frontend/todo_redux.jsx
@@ -1,5 +1,5 @@
 import configureStore from "./store/store.js";
-import { receiveTodo, receiveTodos } from "./actions/todo_actions";
+import { receiveTodo, receiveTodos, removeTodo } from "./actions/todo_actions";
 import Root from "./components/root";
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -7,8 +7,9 @@ import { allTodos } from './reducers/selectors';
 
 // yep this is the entry file now you actually know all your code fucking dope
 const store = configureStore();
-window.receiveTodo = receiveTodo;
+window.receiveTodo = receiveTodo; // omg yes it IS because they're here that it works LOL
 window.receiveTodos = receiveTodos;
+window.removeTodo = removeTodo;
 window.store = store;
 window.allTodos = allTodos;
 window.React = React; // shouldn't need this line but something breaks in todo_list_container and todo_list without it look into it more hmm
