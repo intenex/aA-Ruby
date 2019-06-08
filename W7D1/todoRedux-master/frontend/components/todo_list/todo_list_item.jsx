07463af9b60ab05ba750b4d46dc7e76b72d1fdc7
Ben Yu <intenex@gmail.com>
2019-06-08 15:41:44 -0700

    Bind handleRemove remember in the constructor you put this. things

--- W7D1/todoRedux-master/frontend/components/todo_list/todo_list_item.jsx
+++ W7D1/todoRedux-master/frontend/components/todo_list/todo_list_item.jsx
@@ -3,11 +3,22 @@ import React from 'react';
 class TodoListItem extends React.Component {
   constructor(props) {
     super(props);
+    this.handleRemove = this.handleRemove.bind(this);
+  }
+
+  handleRemove(event) {
+
   }
 
   render() {
     return (
-      <li>{this.props.todo.title}</li>
+      <li>
+        <div>
+          <h3>{this.props.todo.title}</h3>
+          <button onClick={this.handleRemove}>Delete Todo</button>
+          <button>Done</button>
+        </div>
+      </li>
     );
   }
 
