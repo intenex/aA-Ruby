0fdf5e5ed3a96e48fde31f705073825987ba23ee
Ben Yu <intenex@gmail.com>
2019-10-04 22:05:15 -0700

    Finish webpack config

--- W6D5/npm/webpack.config.js
+++ W6D5/npm/webpack.config.js
@@ -1,2 +1,26 @@
 let path = require('path');
 
+module.exports = {
+  entry: './entry.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
