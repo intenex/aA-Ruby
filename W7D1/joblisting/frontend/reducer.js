e37edd7bcc8478834a3427158a957454d159c261
Ben Yu <intenex@gmail.com>
2019-10-06 20:40:47 -0700

    Create a reducer

--- W7D1/joblisting/frontend/reducer.js
+++ W7D1/joblisting/frontend/reducer.js
@@ -1,10 +1,15 @@
+import { merge } from 'lodash';
+
 const initialState = {
   city: "Please Select", 
   jobs: []
 };
 
 const reducer = (state = initialState, action) => {
+  Object.freeze(state);
   switch (action.type) {
+    case "SWITCH_LOCATION":
+      return merge({}, state, { city: action.city, jobs: action.jobs});
     default:
       return state;
   }
