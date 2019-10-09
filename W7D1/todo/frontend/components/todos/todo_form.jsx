ae4ffa02dd44c8c69fdca5327b83c60a469e72cb
Ben Yu <intenex@gmail.com>
2019-10-09 12:10:00 -0700

    Start making the todo form

--- W7D1/todo/frontend/components/todos/todo_form.jsx
+++ W7D1/todo/frontend/components/todos/todo_form.jsx
@@ -4,6 +4,20 @@ import { uniqueId } from './../util.js';
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      title: '',
+      body: ''
+    };
+    this.updateTitle = this.updateTitle.bind(this); // binding creates a scope even when invoked as a callback - how does it do this? Interesting
+    this.updateBody = this.updateBody.bind(this);
+  }
+
+  updateTitle(event) {
+    event.preventDefault();
+  }
+
+  updateBody(event) {
+    event.preventDefault();
   }
 
   render() {
