626ba5299c370ece502a0018ef3aef9e9352b0d8
Ben Yu <intenex@gmail.com>
2019-10-09 13:14:53 -0700

    Add Done and Remove Todo

--- W7D1/todo/frontend/components/todos/todo_list.jsx
+++ W7D1/todo/frontend/components/todos/todo_list.jsx
@@ -4,7 +4,7 @@ import TodoForm from './todo_form';
 
 export default ({todos, receiveTodo, removeTodo}) => (
   <ul>
-    {todos.map((todo, idx) => (<TodoListItem todo={todo} key={idx} removeTodo={removeTodo} />))}
+    {todos.map((todo, idx) => (<TodoListItem todo={todo} key={idx} receiveTodo={receiveTodo} removeTodo={removeTodo} />))}
     <br />
     <TodoForm receiveTodo={receiveTodo}/>
   </ul>
--- W7D1/todo/frontend/components/todos/todo_list_item.jsx
+++ W7D1/todo/frontend/components/todos/todo_list_item.jsx
@@ -3,12 +3,29 @@ import React from 'react';
 class TodoListItem extends React.Component {
   constructor(props) {
     super(props);
+    this.deleteTodo.bind(this);
+    this.updateTodo.bind(this);
+  }
+
+  deleteTodo(event) {
+    event.preventDefault();
+    let { todo, removeTodo } = this.props;
+    removeTodo(todo);
+  }
+
+  updateTodo(event) {
+    event.preventDefault();
+    let { todo, receiveTodo } = this.props;
+    todo.done = !todo.done;
+    receiveTodo(todo);
   }
 
   render() {
-    let { todo, removeTodo } = this.props;
+    let { todo } = this.props;
+    let doneState = "Done";
+    if (todo.done) { doneState = "Undo"; }
     return(
-      <li>{todo.title}</li>
+      <li>{todo.title} -- <button onClick={this.updateTodo}>{doneState}</button> -- <button onClick={this.deleteTodo}>Delete Todo!</button></li>
     );
   }
 }
