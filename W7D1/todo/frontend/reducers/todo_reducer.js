ad020a7b064429c057432cfb4ffdf9309aa536fa
Ben Yu <intenex@gmail.com>
2019-10-08 21:25:03 -0700

    Create initial state for the todo reducer slice

--- W7D1/todo/frontend/reducers/todo_reducer.js
+++ W7D1/todo/frontend/reducers/todo_reducer.js
@@ -1,4 +1,19 @@
-const todosReducer = (state = {}, action) => {
+const initialState = { // note this is just for the todo slice of state as all the reducers are combined by the root reducer love it
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+const todosReducer = (state = initialState, action) => {
   switch (action.type) {
     default:
       return state;
