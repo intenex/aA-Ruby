e257a8f5fc9b6e5827d0f982a0beb8f3371f5ab7
Ben Yu <intenex@gmail.com>
2019-10-08 21:38:32 -0700

    Complete todo_reducer and the whole redux cycle fuck yes

--- W7D1/todo/frontend/reducers/todo_reducer.js
+++ W7D1/todo/frontend/reducers/todo_reducer.js
@@ -1,3 +1,6 @@
+import { RECEIVE_TODOS, RECEIVE_TODO } from './../actions/todo_actions';
+import { merge } from 'lodash';
+
 const initialState = { // note this is just for the todo slice of state as all the reducers are combined by the root reducer love it
   1: {
     id: 1,
@@ -15,6 +18,18 @@ const initialState = { // note this is just for the todo slice of state as all t
 
 const todosReducer = (state = initialState, action) => {
   switch (action.type) {
+    case RECEIVE_TODOS:
+      Object.freeze(state);
+      let todo_objects = {};
+      for (const todo in action.todos) {
+        todo_objects[todo.id] = todo;
+      }
+      return merge({}, todo_objects);
+    case RECEIVE_TODO:
+      Object.freeze(state);
+      let newState = merge({}, state);
+      newState[action.todo.id] = action.todo;
+      return newState;
     default:
       return state;
   }
