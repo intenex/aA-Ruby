1f1f7c2d94215d599fa86ae5a261389d81507600
Ben Yu <intenex@gmail.com>
2019-10-08 21:10:53 -0700

    Get initial rendering working

--- W7D1/joblisting/frontend/entry.jsx
+++ W7D1/joblisting/frontend/entry.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Widget from './components/widget';
 import store from './store';
-import reducer from './reducer';
 
 document.addEventListener("DOMContentLoaded", function(){
   ReactDOM.render(<Widget store={store} />, document.getElementById('root'));
