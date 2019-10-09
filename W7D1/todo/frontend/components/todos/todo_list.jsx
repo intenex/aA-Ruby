026418e37dbb1a4545861ad5a3c9c772d4e17a9d
Ben Yu <intenex@gmail.com>
2019-10-08 23:05:59 -0700

    Start TodoForm

--- W7D1/todo/frontend/components/todos/todo_list.jsx
+++ W7D1/todo/frontend/components/todos/todo_list.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import TodoListItem from './todo_list_item';
+import TodoForm from './todo_form';
 
 export default ({todos, receiveTodo}) => (
   <ul>
     {todos.map((todo, idx) => (<TodoListItem todo={todo} key={idx} />))}
+    <TodoForm receiveTodo={receiveTodo}/>
   </ul>
 );
