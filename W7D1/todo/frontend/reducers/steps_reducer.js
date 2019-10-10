c103221b436af5a23381367bed602bd1c46ecc42
Ben Yu <intenex@gmail.com>
2019-10-10 16:14:30 -0700

    Add sample initial steps state

--- W7D1/todo/frontend/reducers/steps_reducer.js
+++ W7D1/todo/frontend/reducers/steps_reducer.js
@@ -1,7 +1,22 @@
 import { RECEIVE_STEPS, RECEIVE_STEP, REMOVE_STEP } from './../actions/todo_actions':
 import { merge } from 'lodash';
 
-const stepsReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: 'walk to store',
+    done: false,
+    todo_id: 1
+  },
+  2: {
+    id: 2,
+    title: 'buy soap',
+    done: false,
+    todo_id: 1
+  }
+}
+
+const stepsReducer = (state = initialState, action) => {
   const newState = merge({}, state);
   switch (action.type) {
     default:
