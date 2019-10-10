ab8da07d34a4dea36830b7a4f481d325af22e399
Ben Yu <intenex@gmail.com>
2019-10-10 13:59:21 -0700

    Create todo detail view container

--- W7D1/todo/frontend/components/todos/todo_detail_view_container.js
+++ W7D1/todo/frontend/components/todos/todo_detail_view_container.js
@@ -1,2 +1,9 @@
-import React from 'react';
 import { connect } from 'react-redux';
+import TodoDetailView from './todo_detail_view';
+import { removeTodo } from './../../actions/todo_actions';
+
+const mapDispatchToProps = dispatch => ({ // right these are functions that return objects that have keys that become props love it
+  removeTodo: todo => dispatch(removeTodo(todo))
+});
+
+export default connect(null, mapDispatchToProps)(TodoDetailView);
