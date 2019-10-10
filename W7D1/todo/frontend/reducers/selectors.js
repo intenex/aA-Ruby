e1603e871f094c778a0eb277393b30b6abcf1ce0
Ben Yu <intenex@gmail.com>
2019-10-10 16:12:52 -0700

    Make new stepsByTodoId selector

--- W7D1/todo/frontend/reducers/selectors.js
+++ W7D1/todo/frontend/reducers/selectors.js
@@ -1,3 +1,7 @@
 export function allTodos({todo}) { // get the todos slice of state
   return Object.values(todo);
 }
+
+export function stepsByTodoId(state, todoId) {
+  return Object.values(state.steps).filter(step => step.todo_id === todoId); // 
+}
