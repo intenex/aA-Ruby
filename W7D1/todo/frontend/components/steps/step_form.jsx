4d87a661eb13baa8d32e779384982ab38dbf9dee
Ben Yu <intenex@gmail.com>
2019-10-10 18:01:27 -0700

    Implement form in full

--- W7D1/todo/frontend/components/steps/step_form.jsx
+++ W7D1/todo/frontend/components/steps/step_form.jsx
@@ -10,20 +10,27 @@ class StepForm extends React.Component {
     };
     this.updateTitle = this.updateTitle.bind(this);
     this.updateBody = this.updateBody.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   updateTitle(event) {
-
+    event.preventDefault();
   }
 
   updateBody(event) {
+    event.preventDefault();
+  }
 
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
     return(
       <form>
-        
+        Title: <input onChange={this.updateTitle}></input>
+        Details: <input onChange={this.updateBody}></input>
+        <input type="submit" value="Submit" onClick={this.handleSubmit}></input>
       </form>
     );
   }
