60b1bef41d0a606224d4bae49694f5debf462b99
Ben Yu <intenex@gmail.com>
2019-10-10 17:53:59 -0700

    Create presentational component for each step

--- W7D1/todo/frontend/components/steps/step_list_item.jsx
+++ W7D1/todo/frontend/components/steps/step_list_item.jsx
@@ -2,11 +2,36 @@ import React from 'react';
 
 class StepListItem extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.deleteStep = this.deleteStep.bind(this);
+    this.updateStep = this.updateStep.bind(this);
+  }
+
+  deleteStep(event) {
+    event.preventDefault();
+    this.props.removeStep(this.props.step);
+  }
+
+  updateStep(event) {
+    event.preventDefault();
+    const step = this.props.step;
+    step.done = !step.done;
+    this.props.receiveStep(step);
   }
 
   render() {
-    return();
+    const { step } = this.props;
+    let doneState = "Done";
+    if (step.done) {
+      doneState = "Undo";
+    }
+    return(
+      <div>
+        <h3>{step.title}</h3><br/>
+        <p>{step.body}</p><br/>
+        <button onClick={this.updateStep}>{doneState}</button> -- <button onClick={this.deleteStep}>Delete step!</button>
+      </div>
+    );
   }
 }
 
