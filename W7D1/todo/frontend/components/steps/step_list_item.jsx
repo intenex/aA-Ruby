5d7c47f9643f76c91af73406fb5a1539cd34567a
Ben Yu <intenex@gmail.com>
2019-10-10 17:47:31 -0700

    start step list item

--- W7D1/todo/frontend/components/steps/step_list_item.jsx
+++ W7D1/todo/frontend/components/steps/step_list_item.jsx
@@ -1,2 +1,13 @@
 import React from 'react';
 
+class StepListItem extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return();
+  }
+}
+
+export default StepListItem;
