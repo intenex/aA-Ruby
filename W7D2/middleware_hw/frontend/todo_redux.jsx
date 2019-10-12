a40e96b736ad0d568678b6ad2cd8c48b72f80c41
Ben Yu <intenex@gmail.com>
2019-10-11 23:08:11 -0700

    reassign store with all the middlewares applied love it

--- W7D2/middleware_hw/frontend/todo_redux.jsx
+++ W7D2/middleware_hw/frontend/todo_redux.jsx
@@ -18,14 +18,14 @@ function applyMiddlewares(store, ...middlewares) {
   middlewares.forEach(middleware => {
     dispatch = middleware(store)(dispatch); // this returns the final middleware function that just takes an action and finally executes, the dispatch will be the previous middleware each time now brilliant
   });
-  return merge({}, store, { dispatch }); // rewrite dispatch in the
+  return merge({}, store, { dispatch }); // rewrite dispatch in the store to your dispatch yep love it awesome that you build this all yourself now you've forgotten why merge is better than Object.assign some small nuance about assigning recursively children or something like that too right
 } // hmm no semicolon needed here yeah you thought so can't wait to get way more into all of this man programming is just so damn fun
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
-  store.dispatch = addLoggingToDispatch(store); // reassign the dispatch key to your new function hilarious lol amaizng you can do nuts stuff like this yeah amazing anti-pattern to avoid but so awesome you can do it
+  let store = configureStore(preloadedState);
+  store = applyMiddlewares(store, addLoggingToDispatch); // reassign store to the new store with all the middlewares applied love it
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
