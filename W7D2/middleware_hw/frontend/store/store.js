fd83f1073c006c8774db0f85598935e59f11e982
Ben Yu <intenex@gmail.com>
2019-10-11 23:10:22 -0700

    Add middleware to store with applyMiddleware

--- W7D2/middleware_hw/frontend/store/store.js
+++ W7D2/middleware_hw/frontend/store/store.js
@@ -1,8 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
+const addLoggingToDispatch = ({ getState, dispatch }) => next => action => {
+  console.log(`Old State: ${getState()}`);
+  console.log(`Action: ${action}`);
+  const result = next(action);
+  console.log(`New State: ${getState()}`);
+  return result;
+};
+
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
--- W7D2/middleware_hw/frontend/todo_redux.jsx
+++ W7D2/middleware_hw/frontend/todo_redux.jsx
@@ -5,21 +5,14 @@ import { merge } from 'lodash';
 
 import Root from './components/root';
 
-const addLoggingToDispatch = ( {getState, dispatch} ) => next => action => {
-  console.log(`Old State: ${getState()}`);
-  console.log(`Action: ${action}`);
-  const result = next(action);
-  console.log(`New State: ${getState()}`);
-  return result;
-};
 
-function applyMiddlewares(store, ...middlewares) { 
-  let dispatch = store.dispatch;
-  middlewares.forEach(middleware => {
-    dispatch = middleware(store)(dispatch); // this returns the final middleware function that just takes an action and finally executes, the dispatch will be the previous middleware each time now brilliant
-  });
-  return merge({}, store, { dispatch }); // rewrite dispatch in the store to your dispatch yep love it awesome that you build this all yourself now you've forgotten why merge is better than Object.assign some small nuance about assigning recursively children or something like that too right
-} // hmm no semicolon needed here yeah you thought so can't wait to get way more into all of this man programming is just so damn fun
+// function applyMiddlewares(store, ...middlewares) { 
+//   let dispatch = store.dispatch;
+//   middlewares.forEach(middleware => {
+//     dispatch = middleware(store)(dispatch); // this returns the final middleware function that just takes an action and finally executes, the dispatch will be the previous middleware each time now brilliant
+//   });
+//   return merge({}, store, { dispatch }); // rewrite dispatch in the store to your dispatch yep love it awesome that you build this all yourself now you've forgotten why merge is better than Object.assign some small nuance about assigning recursively children or something like that too right
+// } // hmm no semicolon needed here yeah you thought so can't wait to get way more into all of this man programming is just so damn fun
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
