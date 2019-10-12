55689a046cbb0459799aaec096bdf3416a5792c4
Ben Yu <intenex@gmail.com>
2019-10-11 23:13:17 -0700

    Do the bonus and add another middleware

--- W7D2/middleware_hw/frontend/store/store.js
+++ W7D2/middleware_hw/frontend/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
-const addLoggingToDispatch = ({ getState, dispatch }) => next => action => {
+const addLoggingToDispatch = ({ getState }) => next => action => {
   console.log(`Old State: ${getState()}`);
   console.log(`Action: ${action}`);
   const result = next(action);
@@ -9,8 +9,13 @@ const addLoggingToDispatch = ({ getState, dispatch }) => next => action => {
   return result;
 };
 
+const secondMiddleware = ({getState}) => next => action => {
+  console.log(`Next middleware (aka the actual dispatch function since this is the last middleware lol): ${next}`);
+  return next(action);
+};
+
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch, secondMiddleware));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
