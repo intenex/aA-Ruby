9b184e211f846269527e8bf2c277308e97d6c189
Ben Yu <intenex@gmail.com>
2019-10-19 16:11:52 -0700

    Put the thunk action creator fetchSearchGiphys onto the window

--- W7D3/giphy_homework/giphy_search.jsx
+++ W7D3/giphy_homework/giphy_search.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { fetchSearchGiphys } from './util/api_util'; // ah yeah that was your error lmao right need the brackets around it
-import { receiveSearchGiphys } from './actions/giphy_actions';
+// import { fetchSearchGiphys } from './util/api_util'; // ah yeah that was your error lmao right need the brackets around it
+import { receiveSearchGiphys, fetchSearchGiphys } from './actions/giphy_actions';
 
 
 
