b1fd1bf5e7f4db4894c11fc534bc5ddb01b65b4a
Ben Yu <intenex@gmail.com>
2019-10-19 15:25:29 -0700

    Woohoo

--- W7D3/giphy_homework/reducers/giphys_reducer.js
+++ W7D3/giphy_homework/reducers/giphys_reducer.js
@@ -1,1 +1,13 @@
 import { RECEIVE_SEARCH_GIPHYS } from '../actions/giphy_actions';
+import { merge } from './lodash';
+
+const giphysReducer = (state = [], action) => {
+  // Object.freeze(state);
+  // let newState;
+  switch (action.type) {
+    case RECEIVE_SEARCH_GIPHYS:
+      return action.giphys; // just an array awwright
+    default:
+      return state;
+  }
+};
