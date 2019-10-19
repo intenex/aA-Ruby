f96c8f89d0b92ea00cae0d7d3b8f4e79f9286918
Ben Yu <intenex@gmail.com>
2019-10-19 15:53:39 -0700

    Add the dom content loaded in there

--- W7D3/giphy_homework/giphy_search.jsx
+++ W7D3/giphy_homework/giphy_search.jsx
@@ -3,3 +3,9 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 import { fetchSearchGiphys } from './util/api_util'; // ah yeah that was your error lmao right need the brackets around it
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+});
