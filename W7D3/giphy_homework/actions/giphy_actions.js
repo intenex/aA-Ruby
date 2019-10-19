9b66a6da76f2a115444b6c5936171fc952e14618
Ben Yu <intenex@gmail.com>
2019-10-19 16:09:03 -0700

    Dispatch giphys correctly

--- W7D3/giphy_homework/actions/giphy_actions.js
+++ W7D3/giphy_homework/actions/giphy_actions.js
@@ -7,8 +7,9 @@ export const receiveSearchGiphys = giphys => ({
   giphys
 });
 
-export const fetchSearchGiphys = searchTerm => dispatch => { // right thunk action creators return function objects that are passed into the reducer and intercepted by middleware 
-  return new Promise((resolve, reject) => {
-
-  });
+export const fetchSearchGiphys = searchTerm => dispatch => { // right thunk action creators return function objects that are passed into the reducer and intercepted by middleware, generally to run some async action
+  APIUtil.fetchSearchGiphys(searchTerm) // right this is already async they just weren't clear though you can always find ways around it love all of that
+    .then(retrievedGiphys => {
+      dispatch(receiveSearchGiphys(retrievedGiphys));
+    });
 };
