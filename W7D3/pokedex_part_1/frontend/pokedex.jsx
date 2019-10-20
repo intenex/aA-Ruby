b86cc8268230edb54888617c5039ec8c3cf368f7
Ben Yu <intenex@gmail.com>
2019-10-20 16:47:10 -0700

    Test things on the window

--- W7D3/pokedex_part_1/frontend/pokedex.jsx
+++ W7D3/pokedex_part_1/frontend/pokedex.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+import { fetchAllPokemon } from './util/api_util';
 
 document.addEventListener('DOMContentLoaded', () => { // document.addEventListener('DOMContentLoaded', callbackFunc) yep that's how to do it love it everything is on the document in vanillaDOM so great
   const root = document.getElementById('root');
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = fetchAllPokemon;
   ReactDOM.render(<h1>Hi!</h1>, root);
 });
