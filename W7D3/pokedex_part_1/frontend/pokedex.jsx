8bf2c3e056fad771be6bf1eabfb5a60b31b2a3fb
Ben Yu <intenex@gmail.com>
2019-10-20 15:54:53 -0700

    Woohoo

--- W7D3/pokedex_part_1/frontend/pokedex.jsx
+++ W7D3/pokedex_part_1/frontend/pokedex.jsx
@@ -1,2 +1,7 @@
-import react from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => { // document.addEventListener('DOMContentLoaded', callbackFunc) yep that's how to do it love it everything is on the document in vanillaDOM so great
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Hi!</h1>, root);
+});
