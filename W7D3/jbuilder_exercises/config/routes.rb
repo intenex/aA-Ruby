51fc336003c9e89aa769bd4edd31779bdb4648b9
Ben Yu <intenex@gmail.com>
2019-10-20 11:08:43 -0700

    Start the first json extraction love it

--- W7D3/jbuilder_exercises/config/routes.rb
+++ W7D3/jbuilder_exercises/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Your routes here!
   
-  namespace :api, defaults: { format: :json } do # ah amazing you can do it here nice
+  namespace :api, defaults: { format: :json } do # ah amazing you can do it here nice right need the symbol json great
     resources :gifts, only: [ :show ]
     resources :guests, only: [ :index, :show ] do
       resources :gifts, only: [ :index ] # nice you got this right you can put the do after it brilliant
