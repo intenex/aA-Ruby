e53188c0a3badb2d6bafdcfcf5d6013fe33cf984
Ben Yu <intenex@gmail.com>
2019-10-20 16:43:19 -0700

    Create action creator thingy

--- W7D3/pokedex_part_1/frontend/actions/pokemon_actions.js
+++ W7D3/pokedex_part_1/frontend/actions/pokemon_actions.js
@@ -1,6 +1,11 @@
 import * as APIUTIL from './../util/api_util';
 export const RECEIVE_ALL_POKEMON = "RECEIVE_ALL_POKEMON";
 
+export const receiveAllPokemon = pokemon => ({
+  type: RECEIVE_ALL_POKEMON,
+  pokemon
+}); // you really do remember it well at this point so lucky man keep pushing life is so great man so long since you've gotten a haircut since like the beginning of august hilarious 3 months lol
+
 export const fetchAllPokemon = () => dispatch => (
 
 );
