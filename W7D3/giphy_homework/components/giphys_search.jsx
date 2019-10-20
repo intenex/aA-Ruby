10fea778f59d51345f7494a3edfaa92246eb1bb4
Ben Yu <intenex@gmail.com>
2019-10-19 17:26:58 -0700

    Add giphys in a then

--- W7D3/giphy_homework/components/giphys_search.jsx
+++ W7D3/giphy_homework/components/giphys_search.jsx
@@ -6,7 +6,8 @@ export default class GiphysSearch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ""
+      searchTerm: "",
+      giphys: []
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,17 +19,20 @@ export default class GiphysSearch extends React.Component {
   }
 
   handleSubmit(e) { // right this is by default clicked yeah dope hmm look into the solution afterwards yeah spend your time to really dig into and love learning things really cannot wait man life is going to be so great go through interviewcake with Mai and Dom afterwards that's key
-    
+    this.props.fetchSearchGiphys(this.state.searchTerm).then(giphys => this.setState({giphys: giphys}));
   }
 
   render() {
     return(
-      <form>
-        <label>Search Term
-          <input type="text" value={searchTerm} onClick={this.handleChange}/>
-        </label>
-        <input type="submit" value="Search for Giphys!" />
-      </form>
+      <div>
+        <form>
+          <label>Search Term
+            <input type="text" value={searchTerm} onClick={this.handleChange}/>
+          </label>
+          <input type="submit" value="Search for Giphys!" />
+        </form>
+        <GiphysIndex giphys={this.state.giphys}/>
+      </div>
     )
   }
 }
