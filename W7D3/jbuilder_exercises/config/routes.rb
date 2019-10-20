cc7ef8c610041d334d5a86ee8d42f4e8c2683f70
Ben Yu <intenex@gmail.com>
2019-10-20 11:03:16 -0700

    Add all routes appropriately

--- W7D3/jbuilder_exercises/config/routes.rb
+++ W7D3/jbuilder_exercises/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # Your routes here!
   
-  namespace :api, defaults: { format: json } do # ah amazing you can do it here nice
-    resources 
+  namespace :api, defaults: { format: :json } do # ah amazing you can do it here nice
+    resources :gifts, only: [ :show ]
+    resources :guests, only: [ :index, :show ] do
+      resources :gifts, only: [ :index ] # nice you got this right you can put the do after it brilliant
+    end
+    resources :parties, only: [ :index, :show ]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
