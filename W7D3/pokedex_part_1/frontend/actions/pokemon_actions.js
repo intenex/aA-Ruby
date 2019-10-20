42accc19c93a6d75ad53e2cf0cb0789d1c820f60
Ben Yu <intenex@gmail.com>
2019-10-20 16:44:31 -0700

    Install packages

--- W7D3/pokedex_part_1/frontend/actions/pokemon_actions.js
+++ W7D3/pokedex_part_1/frontend/actions/pokemon_actions.js
@@ -7,5 +7,5 @@ export const receiveAllPokemon = pokemon => ({
 }); // you really do remember it well at this point so lucky man keep pushing life is so great man so long since you've gotten a haircut since like the beginning of august hilarious 3 months lol
 
 export const fetchAllPokemon = () => dispatch => (
-
+  dispatch("Hello")
 );
