bc5b9f030a77be137756dfe4388ab9f74bedc15e
Ben Yu <intenex@gmail.com>
2019-10-20 17:00:38 -0700

    Finish the initial pokemon reducer woohoo

--- W7D3/pokedex_part_1/frontend/reducers/pokemon_reducer.js
+++ W7D3/pokedex_part_1/frontend/reducers/pokemon_reducer.js
@@ -3,6 +3,8 @@ import { RECEIVE_ALL_POKEMON } from './../actions/pokemon_actions';
 export default function pokemonReducer(state = {}, action) {
   Object.freeze(state);
   switch (action.type) {
+    case RECEIVE_ALL_POKEMON:
+      return action.pokemon; // the new slice of state should be this I think change it later if the state shape isn't the same
     default:
       return state;
   }
