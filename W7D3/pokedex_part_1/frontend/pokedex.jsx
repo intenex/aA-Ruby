aa45abb11443d5c4a75fbec49f99fc69b31a281d
Ben Yu <intenex@gmail.com>
2019-10-21 22:08:16 -0700

    Remove everything from the window

--- W7D3/pokedex_part_1/frontend/pokedex.jsx
+++ W7D3/pokedex_part_1/frontend/pokedex.jsx
@@ -2,18 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root.jsx';
-import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
-import { fetchAllPokemon } from './util/api_util';
-import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => { // document.addEventListener('DOMContentLoaded', callbackFunc) yep that's how to do it love it everything is on the document in vanillaDOM so great
   const root = document.getElementById('root');
   const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.receiveAllPokemon = receiveAllPokemon;
-  window.requestAllPokemon = requestAllPokemon;
-  window.selectAllPokemon = selectAllPokemon;
-  window.fetchAllPokemon = fetchAllPokemon;
   ReactDOM.render(<Root store={store} />, root);
 });
