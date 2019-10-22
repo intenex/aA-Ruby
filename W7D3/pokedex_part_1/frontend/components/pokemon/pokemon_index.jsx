9df9c0191715a7f3877e3edc77774d4b246e51c4
Ben Yu <intenex@gmail.com>
2019-10-21 21:49:56 -0700

    Right the dispatch should return a function that when invoked calls dispatch with the thing yep love it

--- W7D3/pokedex_part_1/frontend/components/pokemon/pokemon_index.jsx
+++ W7D3/pokedex_part_1/frontend/components/pokemon/pokemon_index.jsx
@@ -5,12 +5,20 @@ export default class PokemonIndex extends React.Component {
     super(props);
   }
 
-  render() {
+  componentDidMount() {
+    console.log(this.props);
+    this.props.requestAllPokemon(); // update state with all the pokemon once the component mounts dope
+  }
+
+  // right you need to return one single parent component simple stupid man
+  render() { // right works great because the json from the jbuilder views api returns the full image_url with the asset_path already so awesome man
     return(
-      {this.props.pokemon.map((pokemon, idx) => <li key={idx}>
-        <p>{pokemon.name}</p><br/>
-        <img src={pokemon.image_url} />
-      </li>)}
+      <ul>
+        {this.props.pokemon.map((pokemon, idx) => (<li key={idx}>
+          <p>{pokemon.name}</p><br/>
+          <img src={pokemon.image_url} />
+        </li>))}
+      </ul>
     );
   }
 }
--- W7D3/pokedex_part_1/frontend/components/pokemon/pokemon_index_container.jsx
+++ W7D3/pokedex_part_1/frontend/components/pokemon/pokemon_index_container.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import PokemonIndex from './pokemon_index';
-import requestAllPokemon from '../../actions/pokemon_actions';
-import selectAllPokemon from '../../reducers/selectors';
+import { requestAllPokemon } from './../../actions/pokemon_actions';
+import { selectAllPokemon } from './../../reducers/selectors';
 
 const mapStateToProps = state => ({
   pokemon: selectAllPokemon(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-  requestAllPokemon: dispatch(requestAllPokemon())
+  requestAllPokemon: () => dispatch(requestAllPokemon())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(PokemonIndex);
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonIndex); // yeah changing file names is a no no apparently lol sigh
