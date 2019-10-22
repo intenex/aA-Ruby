17e953c7f0caad3f06797498ead9626ec5eb4bbb
Ben Yu <intenex@gmail.com>
2019-10-21 22:06:38 -0700

    Remove console.logs

--- W7D3/pokedex_part_1/frontend/components/pokemon/pokemon_index.jsx
+++ W7D3/pokedex_part_1/frontend/components/pokemon/pokemon_index.jsx
@@ -6,14 +6,12 @@ export default class PokemonIndex extends React.Component {
   }
 
   componentDidMount() {
-    // console.log(this.props);
     this.props.requestAllPokemon(); // update state with all the pokemon once the component mounts dope
   }
 
   // right you need to return one single parent component simple stupid man
   render() { // right works great because the json from the jbuilder views api returns the full image_url with the asset_path already so awesome man
     const { pokemon } = this.props;
-    // console.log(pokemon);
     const pokemonList = pokemon.map((pokemon, idx) => {
       return(
       <li key={idx}>
@@ -22,7 +20,6 @@ export default class PokemonIndex extends React.Component {
       </li>
       );
     });
-    // console.log(pokemonList);
     return(
       <ul>
         {pokemonList}
