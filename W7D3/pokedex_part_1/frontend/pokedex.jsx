b9166584795e9effde8c81ce17a35a2dd4ac5b13
Ben Yu <intenex@gmail.com>
2019-10-21 21:22:18 -0700

    Oh now they talk about the Object.values method lol

--- W7D3/pokedex_part_1/frontend/pokedex.jsx
+++ W7D3/pokedex_part_1/frontend/pokedex.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { receiveAllPokemon } from './actions/pokemon_actions';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 import { fetchAllPokemon } from './util/api_util';
 
 document.addEventListener('DOMContentLoaded', () => { // document.addEventListener('DOMContentLoaded', callbackFunc) yep that's how to do it love it everything is on the document in vanillaDOM so great
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => { // document.addEventListen
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.receiveAllPokemon = receiveAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
   window.fetchAllPokemon = fetchAllPokemon;
   ReactDOM.render(<h1>Hi!</h1>, root);
 });
