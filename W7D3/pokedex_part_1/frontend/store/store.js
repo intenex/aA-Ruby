c2e3b07d612ded1eb3f53cc2636152d74399fcf3
Ben Yu <intenex@gmail.com>
2019-10-21 19:23:53 -0700

    Woohoo

--- W7D3/pokedex_part_1/frontend/store/store.js
+++ W7D3/pokedex_part_1/frontend/store/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './../reducers/root_reducer';
 import logger from 'redux-logger';
+import thunk from './../middleware/thunk';
 
 const configureStore = (initialState = {}) => (
-  createStore(rootReducer, initialState, applyMiddleware(logger))
+  createStore(rootReducer, initialState, applyMiddleware(thunk, logger)) // thunk must be first to intercept functions otherwise they get logged by the logger etc
 );
 
 export default configureStore;
