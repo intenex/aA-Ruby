babe3725b1cb80470554a3c6ee5b7dcdb755e952
Ben Yu <intenex@gmail.com>
2019-10-21 21:29:45 -0700

    Create the connected component you do actually know it well now love it keep pushing

---
+++ W7D3/pokedex_part_1/frontend/components/pokemon/pokemon_index_container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PokemonIndex from './pokemon_index';
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonIndex);
