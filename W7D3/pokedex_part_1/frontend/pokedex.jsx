a379a2dd17fdf5455ed151e283bd9c246690e749
Ben Yu <intenex@gmail.com>
2019-10-21 21:35:22 -0700

    Weird not resolving jsx directly ah well

--- W7D3/pokedex_part_1/frontend/pokedex.jsx
+++ W7D3/pokedex_part_1/frontend/pokedex.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import Root from './components/root';
+import Root from './components/root.jsx';
 import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 import { fetchAllPokemon } from './util/api_util';
 import { selectAllPokemon } from './reducers/selectors';
