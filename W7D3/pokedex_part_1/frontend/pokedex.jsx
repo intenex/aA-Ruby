22207677b099eae822b7b9b2d361219d1479527d
Ben Yu <intenex@gmail.com>
2019-10-21 19:16:28 -0700

    Add store to window

--- W7D3/pokedex_part_1/frontend/pokedex.jsx
+++ W7D3/pokedex_part_1/frontend/pokedex.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 import { receiveAllPokemon } from './actions/pokemon_actions';
 import { fetchAllPokemon } from './util/api_util';
 
 document.addEventListener('DOMContentLoaded', () => { // document.addEventListener('DOMContentLoaded', callbackFunc) yep that's how to do it love it everything is on the document in vanillaDOM so great
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.receiveAllPokemon = receiveAllPokemon;
   window.fetchAllPokemon = fetchAllPokemon;
   ReactDOM.render(<h1>Hi!</h1>, root);
