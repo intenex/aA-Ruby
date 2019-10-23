1890ed3277fbde94e9e71ccf28b173caf73a53b0
Ben Yu <intenex@gmail.com>
2019-10-23 10:57:10 -0700

    Create Pokemon Index Item add it later

--- W7D4/pokedex_part_2/frontend/components/pokemon/pokemon_index_item.jsx
+++ W7D4/pokedex_part_2/frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,1 +1,11 @@
 import React from 'react';
+
+// right this isn't the thing that needs the index it's the thing above that does love that lol
+const PokemonIndexItem = ({pokemon}) => (
+  <li>
+    <p>{pokemon.name}</p><br />
+    <img src={pokemon.image_url} />
+  </li>
+);
+
+export default PokemonIndexItem;
