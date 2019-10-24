dde86d3bd03151c1d6986a9139fb60727d881622
Ben Yu <intenex@gmail.com>
2019-10-23 19:34:50 -0700

    Link things

--- W7D4/pokedex_part_2/frontend/components/pokemon/pokemon_index_item.jsx
+++ W7D4/pokedex_part_2/frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // right this isn't the thing that needs the index it's the thing above that does love that lol
 const PokemonIndexItem = ({pokemon}) => (
   <li>
-    <p>{pokemon.name}</p><br />
-    <img src={pokemon.image_url} />
+    <Link to="/pokemon/"{pokemon.id}>
+      <p>{pokemon.name}</p><br />
+      <img src={pokemon.image_url} />
+    </Link>
   </li>
 );
 
