a36246d0c6ecf3613b194b648d855dc0ecaad0d9
Ben Yu <intenex@gmail.com>
2019-10-26 00:55:10 -0700

    put the sessionReducer inside the root reducer

--- W7D5/auth_project/frontend/reducers/session.js
+++ W7D5/auth_project/frontend/reducers/session.js
@@ -4,3 +4,14 @@ const _nullSession = {
   currentUser: null
 };
 
+export default (state = _nullSession, action) => { // omg you can export default fat arrow functions just not consts love it amazing so great to watch these videos
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, { currentUser: action.user });
+    case LOGOUT_CURRENT_USER:
+        return _nullSession; // just return the original null state awesome
+    default:
+      return state;
+  }
+};
