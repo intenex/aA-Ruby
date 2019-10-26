09c15159da471802a2b1d545e41796581c2313bc
Ben Yu <intenex@gmail.com>
2019-10-26 12:30:38 -0700

    Create the signup form more

--- W7D5/auth_project/frontend/components/session/signup.jsx
+++ W7D5/auth_project/frontend/components/session/signup.jsx
@@ -9,6 +9,19 @@ export default class Signup extends React.Component {
       password: ''
     };
   }
+
+  handleInput(type) {
+    return e => {
+      this.setState({ [type]: e.target.value }); // square brackets mean it'll be evaluated before it sets the key brilliant, the variable will be evaluated so great
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createNewUser(this.state)
+      .then(() => this.props.history.push('/chirps'));
+  }
+
   render() {
     return (
       <div className="session-form"></div>
