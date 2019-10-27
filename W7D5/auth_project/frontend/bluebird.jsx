f4a036de6bbd1c63c685438da80674f847b10c02
Ben Yu <intenex@gmail.com>
2019-10-26 23:04:56 -0700

    Woohoo

--- W7D5/auth_project/frontend/bluebird.jsx
+++ W7D5/auth_project/frontend/bluebird.jsx
@@ -5,7 +5,7 @@ import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  let preloadedState = undefined; // right so if no preloadedState it doesn't do anything undefined will just not actually pass anything hmm
+  let preloadedState = undefined; // right so if no preloadedState it doesn't do anything undefined will just not actually pass anything hmm right otherwise everytime you loaded it wouldn't actually fetch the logged in user from the server right now it just loads the initial state of nothing
   if (window.currentUser) {
     preloadedState = {
       session: {
